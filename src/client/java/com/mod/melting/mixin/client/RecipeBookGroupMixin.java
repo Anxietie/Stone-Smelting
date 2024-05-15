30c172c1fa736a39706bf57b0ddd9ae9dba956fe
Anxietie <84636109+Anxietie@users.noreply.github.com>
2024-05-15 13:51:15 -0400

    minor tweaks

--- src/client/java/com/mod/melting/mixin/client/RecipeBookGroupMixin.java
+++ src/client/java/com/mod/melting/mixin/client/RecipeBookGroupMixin.java
@@ -8,10 +8,8 @@ import net.minecraft.recipe.book.RecipeBookCategory;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -31,16 +29,13 @@ public abstract class RecipeBookGroupMixin {
 	@Mutable
 	public static @Final Map<RecipeBookGroup, List<RecipeBookGroup>> SEARCH_MAP;
 
-	@Inject(method = "<clinit>", at = @At("TAIL"))
-	private static void clinit(CallbackInfo ci) {
-		Map<RecipeBookGroup, List<RecipeBookGroup>> copy = new HashMap<>(Map.copyOf(SEARCH_MAP));
-		copy.put(MELTER_SEARCH, ImmutableList.of(MELTER_BLOCKS));
-		SEARCH_MAP = ImmutableMap.copyOf(copy);
-	}
-
 	@Inject(method = "getGroups", at = @At("HEAD"), cancellable = true)
 	private static void getGroups(RecipeBookCategory category, CallbackInfoReturnable<List<RecipeBookGroup>> cir) {
 		if (category == ClassTinkerers.getEnum(RecipeBookCategory.class, "MELTER"))
 			cir.setReturnValue(MELTER);
+	}
+
+	static {
+		SEARCH_MAP = ImmutableMap.<RecipeBookGroup, List<RecipeBookGroup>>builder().putAll(SEARCH_MAP).put(MELTER_SEARCH, ImmutableList.of(MELTER_BLOCKS)).build();
 	}
 }
--- src/main/java/com/mod/melting/mixin/RecipeBookOptionsMixin.java
+++ src/main/java/com/mod/melting/mixin/RecipeBookOptionsMixin.java
@@ -2,16 +2,14 @@ package com.mod.melting.mixin;
 
 import com.chocohead.mm.api.ClassTinkerers;
 import com.google.common.collect.ImmutableMap;
-import com.mod.melting.registry.RecipeTypeRegister;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.recipe.book.RecipeBookCategory;
 import net.minecraft.recipe.book.RecipeBookOptions;
-import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Mixin(RecipeBookOptions.class)
@@ -20,10 +18,7 @@ public class RecipeBookOptionsMixin {
 	@Mutable
 	private static @Final Map<RecipeBookCategory, Pair<String, String>> CATEGORY_OPTION_NAMES;
 
-	@Inject(method = "<clinit>", at = @At("TAIL"))
-	private static void clinit(CallbackInfo ci) {
-		Map<RecipeBookCategory, Pair<String, String>> copy = new HashMap<>(Map.copyOf(CATEGORY_OPTION_NAMES));
-		copy.put(ClassTinkerers.getEnum(RecipeBookCategory.class, "MELTER"), Pair.of("isMelterGuiOpen", "isMelterFilteringCraftable"));
-		CATEGORY_OPTION_NAMES = ImmutableMap.copyOf(copy);
+	static {
+		CATEGORY_OPTION_NAMES = ImmutableMap.<RecipeBookCategory, Pair<String, String>>builder().putAll(CATEGORY_OPTION_NAMES).put(ClassTinkerers.getEnum(RecipeBookCategory.class, "MELTER"), Pair.of("isMelterGuiOpen", "isMelterFilteringCraftable")).build();
 	}
 }
